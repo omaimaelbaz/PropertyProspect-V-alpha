c182fe2f2d6043ffe08a56549e839b82a584b725
omaimaelbaz <oumacode01@gmail.com>
2024-04-13 20:48:36 +0200

    add what we offer  section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -306,7 +306,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h2 class="text-secondary double-down-line text-center mb-5">What We Do</h2>
+                    <h2 class=" text-dark  text-center mb-5 " style="color: #000000;">What We Do</h2>
                 </div>
             </div>
             <div class="text-box-one">
@@ -344,6 +344,84 @@
         </div>
     </div>
     <!-- ======= end what we do section ======= -->
+
+
+
+    <!-- =======  What We Offer section ======= -->
+
+    <section class="w3l-services2" id="services">
+        <div class="features-with-17_sur py-5">
+            <div class="container py-md-3">
+                <div class="heading text-center mx-auto">
+                    <h3 class="head text-black">What We Offer</h3>
+                    <p class="my-3 head ">Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla mollis dapibus nunc, ut rhoncus turpis sodales quis. Integer sit amet mattis quam.</p>
+                </div>
+                <div class="row pt-5 mt-3">
+                    <div class="col-lg-5 features-with-17-left_sur">
+                        <h4 class="lft-head text-dark">We’re Offering Unmatched Services</h4>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, dolore. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, dolore. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla mollis dapibus nunc Integer sit amet mattis quam.</p>
+                        <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla mollis dapibus nunc, ut rhoncus turpis sodales quis. Integer sit amet mattis quam.</p>
+                        <div class="stats-1 text text-info">
+                            <div class="stats-1-left">
+                                <h4 class="text-black-50">2300</h4>
+                                <h6 class="text-black-50">Clients</h6>
+                            </div>
+                            <div class="stats-1-left">
+                                <h4 class="text-black-50">16</h4>
+                                <h6 class="text-black-50">Awards</h6>
+                            </div>
+                            <div class="stats-1-left">
+                                <h4 class="text-black-50">2536</h4>
+                                <h6 class="text-black-50">Projects</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-7 my-lg-0 my-5 align-self-center features-with-17-right_sur">
+                        <div class="features-with-17-right-tp_sur">
+                            <div class="features-with-17-left1">
+                                <span class="fas fa-laptop s4"></span>
+                            </div>
+                            <div class="features-with-17-left2">
+                                <h6><a href="#url">Fastest Service</a></h6>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, dolore.</p>
+                            </div>
+                        </div>
+                        <div class="features-with-17-right-tp_sur">
+                            <div class="features-with-17-left1">
+                                <span class="fas fa-database s5"></span>
+                            </div>
+                            <div class="features-with-17-left2">
+                                <h6><a href="#url">Largest Real Estate</a></h6>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, dolore.</p>
+                            </div>
+                        </div>
+                        <div class="features-with-17-right-tp_sur">
+                            <div class="features-with-17-left1">
+                                <span class="fas fa-lock s3"></span>
+                            </div>
+                            <div class="features-with-17-left2">
+                                <h6><a href="#url">Property Insurance</a></h6>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, dolore.</p>
+                            </div>
+                        </div>
+                        <div class="features-with-17-right-tp_sur">
+                            <div class="features-with-17-left1">
+                            </div>
+                            <span class="fas fa-door-open s2 text-success"></span>
+                            <div class="features-with-17-left2">
+                                <h6><a href="#url">Doorstep Process</a></h6>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, dolore.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- ======= end What We Offer section ======= -->
+
 
 
 
