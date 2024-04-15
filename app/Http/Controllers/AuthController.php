03d7e923b755b766d8721b6d9955036fb9327ecd
omaimaelbaz <oumacode01@gmail.com>
2024-04-15 03:42:34 +0200

    preapre register and login fom

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use  App\Http\Controllers\UserController;
 
 class AuthController extends Controller
 {
-    //
+    public function registerform()
+    {
+
+    }
+    public function loginform()
+    {
+
+    }
+    public function register()
+    {
+
+    }
+    public function login()
+    {
+        
+    }
 }
