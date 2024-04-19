7aa5a54d58c306ea807e5361b2b453fc770c0489
omaimaelbaz <oumacode01@gmail.com>
2024-04-19 18:13:29 +0200

    migrate properties table

--- realestatProject/database/migrations/2024_04_19_144210_create_properties_table.php
+++ realestatProject/database/migrations/2024_04_19_144210_create_properties_table.php
@@ -13,6 +13,25 @@ return new class extends Migration
     {
         Schema::create('properties', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('city');
+            $table->string('country')->default('morocoo');
+            $table->string('postal_code');
+            $table->enum('property_type');
+            $table->integer('size_area')->nullable();
+            $table->integer('num_bedrooms')->nullable();
+            $table->integer('num_bathrooms')->nullable();
+            $table->enum('status', ['sale', 'soldout', 'rente'])->default('rent');
+            $table->decimal('price')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamp('listed_date')->nullable();
+            $table->unsignedBigInteger('agent_id')->nullable();
+            $table->unsignedBigInteger('listed_by'); // User ID of the person who listed the property
+            $table->timestamps();
+
+            $table->foreign('agent_id')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('listed_by')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
